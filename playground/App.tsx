1abdcd092195d7375a41fff2ca8d13a6dfe2f4d5
Ava <ava.inroot@gmail.com>
2026-03-09 18:51:29 +0300

    Fix playground page

--- playground/App.tsx
+++ playground/App.tsx
@@ -6,12 +6,12 @@ export default function App() {
   const [value, setValue] = useState(testMd);
   return (
     <div className="container h-screen mx-auto py-6 flex *:flex-1 px-4">
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 py-4">
         <div className="flex flex-col gap-2">
           <h1 className="text-2xl font-medium">MDEditor Playground</h1>
           <div>
             Development playground for @rootflow/mdeditor — markdown editor with
-            live preview
+            live preview:
           </div>
         </div>
         <MDEditor value={value} onChange={setValue} />

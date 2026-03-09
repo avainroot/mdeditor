56ed3026cac743563925ae631749a3a28d742691
Ava <ava.inroot@gmail.com>
2026-03-09 18:41:32 +0300

    Playground styling

--- playground/App.tsx
+++ playground/App.tsx
@@ -5,8 +5,17 @@ import testMd from "./assets/test.md?raw";
 export default function App() {
   const [value, setValue] = useState(testMd);
   return (
-    <div className="container h-screen mx-auto py-6 flex *:flex-1">
-      <MDEditor value={value} onChange={setValue} />
+    <div className="container h-screen mx-auto py-6 flex *:flex-1 px-4">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-medium">MDEditor Playground</h1>
+          <div>
+            Development playground for @rootflow/mdeditor — markdown editor with
+            live preview
+          </div>
+        </div>
+        <MDEditor value={value} onChange={setValue} />
+      </div>
     </div>
   );
 }
